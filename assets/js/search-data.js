e21d59e670634301c176c3bb8b1011cdc17024a1
Abecid <41438776+Abecid@users.noreply.github.com>
2026-02-21 05:01:29 +0000

    Deploying to gh-pages from @ Abecid/abecid.github.io@329ea7140bc6249428dfe3414f1ae5d3293031b8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,13 +409,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-came-back-to-berkeley",
+          title: 'I came back to Berkeley',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
